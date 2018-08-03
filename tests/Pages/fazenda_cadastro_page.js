971f08de65ae8e87673770be66db427fa2809ed1
Gerry <gerry.con@hotmail.com>
2018-08-03 18:12:46 -0300

    Iniciando projet

---
+++ tests/Pages/fazenda_cadastro_page.js
@@ -0,0 +1,9 @@
+const Fazenda_cadastro = {
+    nome: 'input[name="nome"]',
+    nomeReduzido: 'input[name="nome_reduzido"]',
+    descricao: 'textarea[name="descricao"]',
+    cidade: 'input[name="cidade"]',
+    estado: 'input[name="estado"]',
+    btn_salvar: '#btn_salvar'
+}
+export default Fazenda_cadastro;
